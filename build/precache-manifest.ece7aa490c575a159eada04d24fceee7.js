f6d3ef2f17643ed3e69a9b264c6f9ab1c0d36283
psarmientom <psarmiento@miami.edu>
2022-04-27 13:59:48 -0400

    build

---
+++ build/precache-manifest.ece7aa490c575a159eada04d24fceee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ac91817f358c55ad29e",
+    "url": "/bs-app-new/static/css/main.1c462646.chunk.css"
+  },
+  {
+    "revision": "8ac91817f358c55ad29e",
+    "url": "/bs-app-new/static/js/main.8ac91817.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/bs-app-new/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "c09d97cda67345c8ee40",
+    "url": "/bs-app-new/static/js/2.c09d97cd.chunk.js"
+  },
+  {
+    "revision": "c127db331071d1dc2e57",
+    "url": "/bs-app-new/static/js/runtime~main.c127db33.js"
+  },
+  {
+    "revision": "dabe0bffa2b30ac4cd27ab991f83ac1f",
+    "url": "/bs-app-new/index.html"
+  }
+];
